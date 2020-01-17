fd924a39b2107b493a53cce57ad7831b65cc1740
neverix <nev@neverix.io>
2020-01-17 19:36:16 +0000

    🌿🌔 Checkpoint ./app/lines.js:10453528/397 ./app/360-image.html:10453528/43

--- app/lines.js
+++ app/lines.js
@@ -4,17 +4,16 @@ AFRAME.registerComponent('line-renderer', {
     cursorId: {default: "cursor"}
   },
   init: function() {
-    this.geometry = new THREE.BufferGeometry();
+    this.geometry = new THREE.Geometry();
     this.material = new THREE.LineBasicMaterial({ color: 0x0000ff });
     this.line = new THREE.Line(this.geometry, this.material);
-    this.object3d.add(this.line);
+    this.el.setObject3D(this.attrName, this.line);
     this.cursor = document.getElementById(this.data.cursorId);
   },
   tick: function(t) {
     if(!this.data.enabled) return;
     var worldPos = new THREE.Vector3();
-    worldPos.setFromMatrixPosition(this.cursor.object3d.matrixWorld);
-    console.log(worldPos);
-    this.geometry.vertices.add(worldPos);
+    worldPos.setFromMatrixPosition(this.cursor.object3D.matrixWorld);
+    this.geometry.vertices.push(worldPos);
   }
 })
