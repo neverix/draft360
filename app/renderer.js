b6721d85007f46ccd29733cc47f60de910931917
neverix <nev@neverix.io>
2020-01-17 22:35:16 +0000

    🍃✨ Checkpoint ./app/360-image.html:10453528/461 ./app/renderer.js:10453528/223 ./app/add-images.html:572335/887

--- app/renderer.js
+++ app/renderer.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent('renderer', {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#000000";
+    this.ctx = ctx;
   }
 });
