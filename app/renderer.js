e1b43881cfc4f9d2774dbc3c1ce1297527e13d69
neverix <nev@neverix.io>
2020-01-17 23:23:16 +0000

    🙅😀 Checkpoint ./app/renderer.js:10453528/632 ./app/360-image.html:10453528/54

--- app/renderer.js
+++ app/renderer.js
@@ -4,11 +4,14 @@ AFRAME.registerComponent('renderer', {
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = "#000000";
-    ctx.strokeWidth = 5;
+    ctx.strokeWidth = 10;
     this.ctx = ctx;
     this.canvas = canvas;
+    this.line = [];
+    this.lines = [this.line];
+    
   },
-  tick: function() {
+  tick: function(t) {
     var rot = this.el.getAttribute("rotation");
     var x = rot.y / 360 + 0.5;
     var y = rot.x / 360 + 0.5;
@@ -17,8 +20,14 @@ AFRAME.registerComponent('renderer', {
     y *= this.canvas.height;
     x = Math.floor(x);
     y = Math.floor(y);
+    this.line.push([x, y]);
+    this.ctx.beginPath();
     this.ctx.moveTo(x, y);
-    this.ctx.lineTo(x, y);
+    this.ctx.lineTo(x + 5, y);
     this.ctx.stroke();
+    console.log(document.getElementById("canvas-360")
+      .getAttribute("material"))
+    document.getElementById("canvas-360")
+      .getAttribute("material").map.needsUpdate = true;
   }
 });
