8c155c95ffd70cf03f9f31a5dd11bcd06e5d2479
neverix <nev@neverix.io>
2020-01-17 22:11:16 +0000

    🐷🎹 Checkpoint ./app/renderer.js:10453528/1478 ./app/360-image.html:10453528/447

--- app/renderer.js
+++ app/renderer.js
@@ -1,13 +1,9 @@
 AFRAME.registerComponent('renderer', {
-  schema: {
-    backdrop: { default: "https://cdn.glitch.com/dff38557-346e-4aa3-94d5-969225a03cf0%2Fpuydesancy.jpg" }
-  },
   init: function() {
-    document.getElementById("backdrop-img").src = this.data.backdrop;
-    this.backdrop = document.getElementById("backdrop").getProperty("sky");
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(50, 50, 60, 60)
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 });
