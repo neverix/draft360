a3c4ec1cd6e2a297be31bb13305ecd81fffe0b75
neverix <nev@neverix.io>
2020-01-17 22:47:17 +0000

    🎊💓 Checkpoint ./app/renderer.js:10453528/916

--- app/renderer.js
+++ app/renderer.js
@@ -3,7 +3,19 @@ AFRAME.registerComponent('renderer', {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "#000000";
+    ctx.fillStyle = "#000000";
     this.ctx = ctx;
+    this.canvas = canvas;
+  },
+  tick: function() {
+    var rot = this.el.getAttribute("rotation");
+    var x = rot.y / 360 + 0.5;
+    var y = rot.x / 360 + 0.5;
+    x -= Math.floor(x);
+    x *= this.canvas.width;
+    y *= this.canvas.height;
+    x = Math.floor(x);
+    y = Math.floor(y);
+    this.ctx.fillRect(x, y, x + 10, y + 10);
   }
 });
