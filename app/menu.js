bba4979a08d0269d1ae88cfec000af030c0919e6
neverix <nev@neverix.io>
2020-01-19 16:41:26 +0000

    ⛳️⛩ Checkpoint ./app/style.css:10453528/136 ./app/index.html:10453528/550 ./app/loading.js:10453528/54 ./app/menu.js:10453528/691

--- app/menu.js
+++ app/menu.js
@@ -11,6 +11,27 @@ function getDialog() {
   return dialog;
 }
 
+function showTextDialog(text) {
+  if(!isTouch()) {
+    document.getElementById("camera").components
+      .mylookcontrols.movementMode = true;
+  }
+  var dialog = getDialog();
+  dialog.showModal();
+  document.getElementById("modal-text").innerHTML = text;
+  var buttonsElem = document.getElementById("modal-buttons");
+  buttonsElem.innerHTML = "";
+  buttons.push(["Close", closeDialog]);
+  buttons.forEach(([text, action]) => {
+    var button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.setAttribute("class", "mdl-button");
+    button.innerText = text;
+    button.onclick = action;
+    buttonsElem.appendChild(button);
+  });
+}
+
 function showDialog(text, buttons) {
   if(!isTouch()) {
     document.getElementById("camera").components
