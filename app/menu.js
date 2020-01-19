4204b83c0532b52931ae0a238e740aa0c9c496ff
neverix <nev@neverix.io>
2020-01-19 16:53:23 +0000

    👘🎐 Checkpoint ./app/menu.js:10453528/530 ./app/loading.js:10453528/32 ./app/360-image.html:10453528/1601 ./app/index.html:7853388/26

--- app/menu.js
+++ app/menu.js
@@ -12,24 +12,9 @@ function getDialog() {
 }
 
 function showTextDialog(text) {
-  if(!isTouch()) {
-    document.getElementById("camera").components
-      .mylookcontrols.movementMode = true;
-  }
   var dialog = getDialog();
   dialog.showModal();
   document.getElementById("modal-text").innerHTML = text;
-  var buttonsElem = document.getElementById("modal-buttons");
-  buttonsElem.innerHTML = "";
-  buttons.push(["Close", closeDialog]);
-  buttons.forEach(([text, action]) => {
-    var button = document.createElement("button");
-    button.setAttribute("type", "button");
-    button.setAttribute("class", "mdl-button");
-    button.innerText = text;
-    button.onclick = action;
-    buttonsElem.appendChild(button);
-  });
 }
 
 function showDialog(text, buttons) {
