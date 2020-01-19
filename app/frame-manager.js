deb7756000748b91d1d2ffa139535b8c7a3ccdb3
neverix <nev@neverix.io>
2020-01-19 03:39:48 +0000

    🕵🎅 Checkpoint ./app/look.js:10453528/350 ./app/frame-manager.js:10453528/373

--- app/frame-manager.js
+++ app/frame-manager.js
@@ -88,6 +88,13 @@ AFRAME.registerComponent('frame-manager', {
       ]);
       showDialog("Frame:", buttons);
     };
+    this.imageMode = false;
+    document.getElementById("image-mode").onclick = () => {
+      this.imageMode = true;
+      // this.stampImage = new Image();
+      // this.stampImage.crossOrigin = "anonymous";
+      // this.stampImage.src = "https://cdn.glitch.com/dff38557-346e-4aa3-94d5-969225a03cf0%2Fstamp_person1.png?v=1579396695751";
+    }
   },
   tick: function() {
     if(!this.loaded) return;
