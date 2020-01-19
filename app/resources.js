7b64125d3d3eacbc27cd4a5ecc4d4b2713d668b0
neverix <nev@neverix.io>
2020-01-19 04:15:48 +0000

    🐊🏄 Checkpoint ./app/frame-manager.js:10453528/2617 ./app/360-image.html:10453528/1072 ./app/resources.js:10453528/947

--- app/resources.js
+++ app/resources.js
@@ -13,4 +13,17 @@ var images = [
     "Person",
     "https://cdn.glitch.com/dff38557-346e-4aa3-94d5-969225a03cf0%2Fstamp_person1.png?v=1579396695751"  
   ]
-]
+];
+
+function pickFile() {
+  var input = document.createElement("file")
+}
+
+window.addEventListener("load", () => {
+  document.getElementById("360-open").addEventListener("click", () => {
+    console.log("360");
+  });
+  document.getElementById("image-open").addEventListener("click", () => {
+    console.log("image");
+  });
+});
