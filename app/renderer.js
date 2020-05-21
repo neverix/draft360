7b9f6cf54d249ef9beb54b1ec09fb627fea87815
neverix <nev@neverix.io>
2020-05-21 07:56:03 +0000

    🐘🏫 Checkpoint ./app/renderer.js:10453528/2240 ./app/360-image.html:10453528/27

--- app/renderer.js
+++ app/renderer.js
@@ -9,66 +9,28 @@ AFRAME.registerComponent('renderer', {
     enabled: {default: true}
   },
   deleteFrame: function(scene) {
-    this.canvases.splice(scene, 1);
-    this.images.splice(scene, 1);
-    this.meshes.splice(scene, 1);
-    this.textures.splice(scene, 1);
-    this.ctxs.splice(scene, 1);
+    this.lines.splice(scene, 1);
+    this.geos.splice(scene, 1);
   },
   loadImage: function(img, scene) {
+    this.sky.setAttribute("src", img);
     var mat = new THREE.LineBasicMaterial({
       color: this.data.strokeColor,
       linewidth: this.data.strokeSize
     });
-    if(this.images[scene]) {
-      this.image = this.images[scene];
-      if(!!this.meshes[scene] && !!this.lines[scene]) {
-        this.loaded = true;
-        this.el.setObject3D("mesh", this.meshes[scene]);
-        var geo = new THREE.Geometry();
-        this.line = this.lines[scene];
-        geo.vertices = this.line;
-        geo.verticesNeedUpdate = true;
-        this.geo = new THREE.LineSegments(geo, mat);
-        this.geos[scene] = this.geo;
-        this.el.setObject3D("lines", this.geo);
-      }
-      return;
+    if(!this.geos[scene]) {
+      
     }
-    this.image = new Image();
-    this.image.crossOrigin = "anonymous";
-    this.image.src = img;
-    this.loaded = false;
-    this.images[scene] = this.image;
-    this.image.onload = (() => {
-      this.texture = new THREE.Texture(this.image);
-      this.textures[scene] = this.texture;
-      var material = new THREE.MeshBasicMaterial({
-        map: this.texture,
-        flatShading: THREE.FlatShading,
-        side: THREE.BackSide
-      });
-      var geometry = new THREE.SphereGeometry(this.data.radius,
-                                              this.data.subdiv,
-                                              this.data.subdiv);
-      var mesh = new THREE.Mesh(geometry, material);
-      this.meshes[scene] = mesh;
-      this.el.setObject3D("mesh", mesh);
-      var geo = new THREE.Geometry();
-      this.line = this.lines[scene]
-      geo.vertices = this.line;
-      this.geo = new THREE.LineSegments(geo, mat);
-      this.geos[scene] = this.geo;
-      this.el.setObject3D("lines", this.geo);
-      this.loaded = true;
-      this.texture.needsUpdate = true;
-    }).bind(this);
+    var geo = new THREE.Geometry();
+    this.line = this.lines[scene]
+    geo.vertices = this.line;
+    this.geo = new THREE.LineSegments(geo, mat);
+    this.geos[scene] = this.geo;
+    this.el.setObject3D("mesh", this.geo);
+    this.loaded = true;
   },
   init: function() {
     this.loaded = false;
-    this.images = [];
-    this.meshes = [];
-    this.textures = [];
     this.line = [];
     this.lines = [[]];
     this.geos = [];
@@ -76,7 +38,7 @@ AFRAME.registerComponent('renderer', {
     this.enabled = false;
     this.prevPos = null;
     this.prevEnabled = false;
-    this.sphere = document.getElementById("sphere")
+    this.sky = document.getElementById("sky")
   },
   tick: function(t) {
     var pos = document.getElementById("cursor").components.raycaster.raycaster.ray.direction;
