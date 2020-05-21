28138f4be237b4ed777f99f347a7b66809059a68
neverix <nev@neverix.io>
2020-05-21 14:35:04 +0000

    Rewound to commit af8cf9d954750a177c96811c50cfc4e6c73e73d9 + 0 edits

--- app/renderer.js
+++ app/renderer.js
@@ -13,52 +13,45 @@ AFRAME.registerComponent('renderer', {
     this.geos.splice(scene, 1);
   },
   loadImage: function(img, scene) {
-    this.loaded = false;
     this.sky.setAttribute("src", img);
+    var mat = new THREE.LineBasicMaterial({
+      color: this.data.strokeColor,
+      linewidth: this.data.strokeSize
+    });
     if(!this.geos[scene]) {
-      var mat = new THREE.LineBasicMaterial({
-        color: this.data.strokeColor,
-        linewidth: this.data.strokeSize
-      });
-      var geo = new THREE.Geometry();
-      this.line = geo.vertices;
-      this.lines[scene] = this.line;
-      this.geo = new THREE.LineSegments(geo, mat);
-      this.geos[scene] = this.geo;
-      this.el.setObject3D("line", this.geo);
-      geo.verticesNeedUpdate = true;
+      
     }
-    this.geo = this.geos[scene];
-    this.line = this.lines[scene];
+    var geo = new THREE.Geometry();
+    this.line = this.lines[scene]
+    geo.vertices = this.line;
+    this.geo = new THREE.LineSegments(geo, mat);
+    this.geos[scene] = this.geo;
+    this.el.setObject3D("mesh", this.geo);
     this.loaded = true;
   },
   init: function() {
     this.loaded = false;
     this.line = [];
     this.lines = [[]];
-    this.geo = null;
     this.geos = [];
     this.circle = document.getElementById("circle");
     this.enabled = false;
     this.prevPos = null;
-    this.sky = document.getElementById("sky");
-    this.eraserMode = false;
-    document.getElementById("edit-mode").onclick = () => {
-      this.eraserMode = false;
-    }
-    document.getElementById("eraser-mode").onclick = () => {
-      this.eraserMode = true;
-    }
+    this.prevEnabled = false;
+    this.sky = document.getElementById("sky")
   },
   tick: function(t) {
     var pos = document.getElementById("cursor").components.raycaster.raycaster.ray.direction;
     pos = new THREE.Vector3(pos.x, pos.y, pos.z);
     document.getElementById("cover").style.display = this.loaded ? "none" : "block";
-    if(this.loaded && this.enabled && !!this.prevPos) {
+    if(this.prevEnabled && !this.enabled) {
+      
+    }
+    if(this.loaded && this.enabled) {
       this.line.push(this.prevPos, pos);
       this.geo.geometry.verticesNeedUpdate = true;
-      this.geo.geometry.elementsNeedUpdate = true;
     }
     this.prevPos = pos;
+    this.prevEnabled = this.enabled;
   }
 });
